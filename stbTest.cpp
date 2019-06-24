1669e5c6c904a986a796556002644ef8f87e1602
Rachel Wang <43324267+MemoriZzz@users.noreply.github.com>
2019-06-23 23:23:03 -0400

    Add files via upload
    Texture 1

---
+++ stbTest.cpp
@@ -0,0 +1,20 @@
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); //OpenGL和手提包库在y轴上是反向的
+	unsigned char* data = stbi_load("pic.png", &width, &height, &nrChannels, 0);
+	
+	for (size_t i = 0; i < 50; i++)
+	{
+		cout << (int)data[i] << endl;
+	}
+
+	stbi_image_free(data);
+
+	return 0;
+}
